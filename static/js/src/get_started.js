39871cce8cb4898a3852a45393f30bfc60677d7e
Bentz Edwards <bedwards@localhost.localdomain>
2015-12-10 12:45:44 -0700

    Small changes made to getting started page.

--- static/js/src/get_started.js
+++ static/js/src/get_started.js
@@ -5,8 +5,8 @@ var GetStarted = React.createClass({
                 <h1>Getting Started</h1>
            		<p>
                     Once you have registered and logged in, you will be able to upload tanks that you have programmed
-                    and battle them against other users. To download the tank interface you will need to implement, click 
-                    <a>here</a>. 
+                    and battle them against other users. Here is an example of the tank interface you will need to implement.   
+                    <a href="/downloads/tanks/wait.java" download> Sitting Duck Tank</a>. For more information, visit our <a href="">wiki page</a>. 
             	</p>
                 <p>
                     Feel like you need to know more Java in order to participate? Click <a href="https://www.codecademy.com/learn/learn-java">here</a> to take a free crash course
