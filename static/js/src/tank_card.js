f91dc135c6c2df26a5ed736fa60f5f0590f8022f
Bentz Edwards <bedwards@localhost.localdomain>
2016-02-25 10:31:14 -0700

    Color changes to the tank_card class

--- static/js/src/tank_card.js
+++ static/js/src/tank_card.js
@@ -13,26 +13,27 @@ var TankCard = React.createClass({
                             <img className="tankImage" src="../../images/BlueEast.gif"></img>
                         </div>
                     </div>
-                    <div className="tankCardSection2">
-                        <div className="tankCardStats">
-                            <span>Wins:</span>
-                            <span>Losses:</span>
-                            <span>Tanks killed:</span>
-                        </div>
-                        <div className="tankCardButton">
+                    <div className="tankCardStats">
+                        <h4>Stats:</h4>
+                        <span>Won:</span>
+                        <span>Lost:</span>
+                        <span>Kills:</span>
+                    </div>
+                    <div className="buttonSection">
                             {inArmory ? 
-                                <button type="submit" className="btn btn-primary" value="Download">Download</button>
+                                <button type="submit" className="btn btn-primary tankCardButton">Upload</button>
                                 : null
                             }
                             {inArmory ?  
-                                <button type="submit" className="btn btn-danger" value="Delete">Delete</button>
+                                <button type="submit" className="btn btn-danger tankCardButton">Download</button>
                                 : null
                             }
                             {!inArmory ?  
-                                <button onClick={this.props.joinGame.bind(null, tank)} ontype="submit" className="btn btn-success btn-block">Join Game</button>
+                                <button onClick={this.props.joinGame.bind(null, tank)} ontype="submit" className="btn btn-success tankCardButton">Join!</button>
                                 : null
-                            }  
-                        </div>
+                            } 
+                            
+                            
                     </div>
                 </div>
         );
